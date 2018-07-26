64fa425c6ad4534587cd64c7f1bcf8dc7cc1aaf8
Victor Velasquez <victorv24@gmail.com>
2018-07-25 18:53:11 -0700

    all info displaying

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    let data = axios.get('http://localhost:3001/fields').then(resp => {
+    axios.get('http://localhost:3001/fields').then(resp => {  
       console.log(resp.data)
 
       this.setState({
@@ -33,15 +33,25 @@ class App extends Component {
             <h1>Sign Up For My Web App</h1>
           </div>
           
-          <form>
+          <form className="form-area">
             {this.state.data.map(data => (
-              // if text, else select, else textarea
-              data.type === 'text' ? <input key={data.id} type={data.type} placeholder={data.label}/>
-                : data.type === 'select' ? <select key={data.id} placeholder={data.label}></select>
+              // if text, else if select, else textarea
+              data.type === 'text' | data.type === 'email' | data.type === 'tel' ? 
+                <input key={data.id} type={data.type} placeholder={data.label}/>
+                : data.type === 'select' ? 
+                  <select key={data.id} id={data.id}>
+                    <option>{data.label}</option>
+                    {data.options.map(opt => (
+                      <option key={opt.id} value={opt.value}>{opt.label}</option>
+                    ))}
+                  </select>
                 : <textarea key={data.id} placeholder={data.label}></textarea>
             ))}
-            <button type="submit">Submit Form</button>
           </form>
+
+          <div className="submit-area">
+              <button type="submit">Submit Form</button>
+            </div>
         </div>
       </div>
     )
